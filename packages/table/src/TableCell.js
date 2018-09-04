0413393d1a29f1dc6507abd616d7cb7419c86d62
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-09-04 15:02:13 -0300

    Fix table styles (#8)
    * Add content width and hover styles
    
    * 1.0.1

--- packages/table/src/TableCell.js
+++ packages/table/src/TableCell.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 import styled from 'react-emotion'
-import {tableCellStyle} from './styles'
+import {tableCellStyle, tableCellClipContentStyle} from './styles'
 
 const TableCell = styled.td`
   ${tableCellStyle};
+  ${tableCellClipContentStyle};
 `
 
 TableCell.propTypes = {
   numeric: PropTypes.bool,
+  minWidth: PropTypes.number,
   children: PropTypes.node,
 }
 
